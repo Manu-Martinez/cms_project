1a7d293987965977045ed3377ee23183abf7a4a4
Manu-Martinez <manu.martinezgalan@gmail.com>
2024-02-03 04:11:53 -0300

    created basic config to connect to db

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db = SQLAlchemy(app)
