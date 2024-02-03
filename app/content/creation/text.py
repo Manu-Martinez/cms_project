79376fba7fdab521e195efe232b942ac4cd428ff
Manu-Martinez <manu.martinezgalan@gmail.com>
2024-02-03 04:12:48 -0300

    added the creating folder with its respective files

---
+++ app/content/creation/text.py
@@ -0,0 +1,16 @@
+from flask import render_template, redirect, url_for, request
+from . import db, create_app
+from .models import Post
+
+app = create_app()
+
+@app.route('/post/create', methods=['GET', 'POST'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_post = Post(title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('create_post.html')
