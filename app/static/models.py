1e69098404c4787217f542aca41cc577afd4e2c3
Manu-Martinez <manu.martinezgalan@gmail.com>
2024-02-13 03:48:00 -0300

    Refactor User model to include password hashing

--- app/static/models.py
+++ app/static/models.py
@@ -1,18 +1,22 @@
+from werkzeug.security import generate_password_hash
 from datetime import datetime
 from app import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
-    links = db.relationship('Link', backref='author', lazy='dynamic')
-    videos = db.relationship('Video', backref='author', lazy='dynamic')
-    photos = db.relationship('Photo', backref='author', lazy='dynamic')
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), index=True, unique=True)
+	email = db.Column(db.String(120), index=True, unique=True)
+	password_hash = db.Column(db.String(128))
+	posts = db.relationship('Post', backref='author', lazy='dynamic')
+	links = db.relationship('Link', backref='author', lazy='dynamic')
+	videos = db.relationship('Video', backref='author', lazy='dynamic')
+	photos = db.relationship('Photo', backref='author', lazy='dynamic')
+	
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+	def __repr__(self):
+		return '<User {}>'.format(self.username)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
