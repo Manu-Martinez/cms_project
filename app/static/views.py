d27742fd742cec2f7a7415c81fd810a1cebc21b5
Manu-Martinez <manu.martinezgalan@gmail.com>
2024-02-13 03:47:54 -0300

    Add registration, login, and logout functionality

--- app/static/views.py
+++ app/static/views.py
@@ -1,32 +1,53 @@
-from flask import render_template, redirect, url_for, request
+from flask import render_template, flash, abort, redirect, url_for, request
 from app import db
 from .models import User, Post
+from .form import RegistrationForm
 from flask import current_app as app
-from flask_login import current_user
+from flask_login import current_user, logout_user, login_user, login_required
+from flask import abort
 
+
+# HOME ROUTE #
 @app.route('/')
 def home():
 	return render_template('home.html')
 
+# REGISTER #
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
+# LOGIN AND LOGOUT #
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user is None or not user.check_password(request.form['password']):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+#GET USER #
 @app.route('/user')
 def no_user():
     return render_template('empty_user.html')
-
-@app.route('/create-user', methods=['GET', 'POST'])
-def create_user():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        # Add more fields as necessary
-
-        user = User(username=username, email=email)
-        db.session.add(user)
-        db.session.commit()
-
-        return redirect(url_for('user', username=username))
-
-    return render_template('create_user.html')
 
 @app.route('/user/<username>')
 def user(username):
@@ -42,12 +63,45 @@ def get_user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
 
+# EDIT USER PROFILE #
+@app.route('/user/<username>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    if user != current_user:
+        abort(403)  # Forbidden access
+    if request.method == 'POST':
+        new_username = request.form['username']
+        if User.query.filter_by(username=new_username).first() is not None:
+            flash('Username already exists. Please choose a different one.')
+            return redirect(url_for('edit_user', username=user.username))
+        user.username = new_username
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user', username=user.username))
+    return render_template('edit_user.html', user=user)
 
 
+# DELETE USER #
+@app.route('/user/<username>/delete', methods=['POST'])
+@login_required
+def delete_user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    if user != current_user:
+        abort(403)  # Forbidden access
+    logout_user()
+    db.session.delete(user)
+    db.session.commit()
+    flash('Your account has been deleted.')
+    return redirect(url_for('home'))
+
+
+#POST ROUTE#
 @app.route('/post')
 def no_post():
     return render_template('empty_post.html')
 
+# CREATE A POST #
 @app.route('/create-post', methods=['GET', 'POST'])
 def create_post():
 	if request.method == 'POST':
@@ -64,11 +118,14 @@ def create_post():
 
 	return render_template('create_post.html')
 
+# GET A POST #
 @app.route('/post/<int:post_id>', methods=['GET'])
 def get_post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', post=post)
 
+
+# EDIT A POST  #
 @app.route('/post/<int:post_id>/edit', methods=['GET', 'POST'])
 def edit_post(post_id):
 	post = Post.query.get_or_404(post_id)
@@ -78,6 +135,8 @@ def edit_post(post_id):
 		return redirect(url_for('user', username=post.author.username))
 	return render_template('edit_post.html', post=post)
 
+
+# DELETE A POST #
 @app.route('/post/<int:post_id>/delete', methods=['POST'])
 def delete_post(post_id):
 	post = Post.query.get_or_404(post_id)
