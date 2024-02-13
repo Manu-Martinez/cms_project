3c04c475b019d9177c8d5d1f38802285af609f17
Manu-Martinez <manu.martinezgalan@gmail.com>
2024-02-13 03:47:34 -0300

    Add Flask-Login functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 import os 
 from dotenv import load_dotenv
+from . import loginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+loginManager = LoginManager()
 
 load_dotenv()
 
@@ -19,9 +22,15 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    loginManager.init_app(app)
 
     with app.app_context():
         from .static import views   # noqa: F401
         db.create_all()  # Create the database tables
 
     return app
+
+@loginManager.user_loader
+def load_user(user_id):
+	from .static.models import User  # Import here to avoid circular import
+	return User.query.get(int(user_id))
