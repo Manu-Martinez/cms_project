15d2992d7f25a20ecefebdb0787de525e8e6f858
Manu-Martinez <manu.martinezgalan@gmail.com>
2024-02-18 03:07:11 -0300

    Add reminder to check and register social media APIs

--- app/publishing/social_media.py
+++ app/publishing/social_media.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from some_social_media_library import SocialMediaAPI
-
+## REMEMBER TO CHECK THE SOCIALMEDIA PY FILE CREATED BY COPILOT.   ##
+## YOU STILL NEED TO REGISTER YOUR APP TO GET THE SOCIAL MEDIA APIS ##
 social_media = Blueprint('social_media', __name__)
 
 api = SocialMediaAPI('api_key')
